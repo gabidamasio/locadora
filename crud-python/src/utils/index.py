386c412c4b57a8dae9ffcf88e5f095347b42dabd
Rayane Cavalcanti <rayaneescola32@gmail.com>
2024-10-30 20:36:53 -0300

    feature: arquitetura e documentacao inicial

---
+++ crud-python/src/utils/index.py
@@ -0,0 +1,23 @@
+teste = 'Teste modularização'
+
+print('teste 2')
+
+def menu(): 
+    print('Bem-vindo a Locadora!')
+    
+def createData():
+    data = ''
+    print('create')
+    
+def updateData():
+    data = ''
+    print('update')
+    
+def readData():
+    data = ''
+    print('read')
+
+def deleteData():
+    data = ''
+    print('delete')
+    
